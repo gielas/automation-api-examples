ad459d1471f9fe64669a4d9867dad40ac31afec4
Svante Strömvall <svante.stromvall@knowit.se>
2022-09-05 12:13:17 +0200

    Switch to Azure in another example

--- nodejs/pulumiOverHttp-ts/index.ts
+++ nodejs/pulumiOverHttp-ts/index.ts
@@ -4,59 +4,86 @@ import {
     StackAlreadyExistsError,
     StackNotFoundError
 } from "@pulumi/pulumi/automation";
-import { s3 } from "@pulumi/aws";
-import { PolicyDocument } from "@pulumi/aws/iam";
+import * as cdn from "@pulumi/azure-native/cdn";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+import * as pulumi from "@pulumi/pulumi";
 import * as express from "express";
-import { autoscaling } from "@pulumi/aws/types/enums";
 
 const projectName = "pulumi_over_http";
 
 // this function defines our pulumi S3 static website in terms of the content that the caller passes in.
 // this allows us to dynamically deploy websites based on user defined values from the POST body.
 const createPulumiProgram = (content: string) => async () => {
-    // Create a bucket and expose a website index document
-    const siteBucket = new s3.Bucket("s3-website-bucket", {
-        website: {
-            indexDocument: "index.html",
-        },
+
+    const resourceGroup = new resources.ResourceGroup("resourceGroup");
+
+    const profile = new cdn.Profile("profile", {
+      resourceGroupName: resourceGroup.name,
+      sku: {
+        name: cdn.SkuName.Standard_Microsoft,
+      },
+    });
+    
+    const storageAccount = new storage.StorageAccount("storageaccount", {
+      enableHttpsTrafficOnly: true,
+      kind: storage.Kind.StorageV2,
+      resourceGroupName: resourceGroup.name,
+      sku: {
+        name: storage.SkuName.Standard_LRS,
+      },
+    });
+    
+    // Enable static website support
+    const staticWebsite = new storage.StorageAccountStaticWebsite("staticWebsite", {
+      accountName: storageAccount.name,
+      resourceGroupName: resourceGroup.name,
+      indexDocument: "index.html",
+    //   error404Document: "404.html",
     });
 
     // here our HTML is defined based on what the caller curries in.
     const indexContent = content;
 
     // write our index.html into the site bucket
-    let object = new s3.BucketObject("index", {
-        bucket: siteBucket,
-        content: indexContent,
-        contentType: "text/html; charset=utf-8",
-        key: "index.html"
+    new storage.Blob("index.html", {
+        resourceGroupName: resourceGroup.name,
+        accountName: storageAccount.name,
+        containerName: staticWebsite.containerName,
+        source: content,
+        contentType: "text/html",
+      })
+    
+    // Web endpoint to the website
+    const staticEndpoint = storageAccount.primaryEndpoints.web;
+    
+    // Optionally, add a CDN.
+    const endpointOrigin = storageAccount.primaryEndpoints.apply((ep) =>
+      ep.web.replace("https://", "").replace("/", "")
+    );
+    const endpoint = new cdn.Endpoint("endpoint", {
+      endpointName: storageAccount.name.apply((sa) => `cdn-endpnt-${sa}`),
+      isHttpAllowed: false,
+      isHttpsAllowed: true,
+      originHostHeader: endpointOrigin,
+      origins: [
+        {
+          hostName: endpointOrigin,
+          httpsPort: 443,
+          name: "origin-storage-account",
+        },
+      ],
+      profileName: profile.name,
+      queryStringCachingBehavior: cdn.QueryStringCachingBehavior.NotSet,
+      resourceGroupName: resourceGroup.name,
     });
-
-    // Create an S3 Bucket Policy to allow public read of all objects in bucket
-    function publicReadPolicyForBucket(bucketName): PolicyDocument {
-        return {
-            Version: "2012-10-17",
-            Statement: [{
-                Effect: "Allow",
-                Principal: "*",
-                Action: [
-                    "s3:GetObject"
-                ],
-                Resource: [
-                    `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
-                ]
-            }]
-        };
-    }
-
-    // Set the access policy for the bucket so all objects are readable
-    let bucketPolicy = new s3.BucketPolicy("bucketPolicy", {
-        bucket: siteBucket.bucket, // refer to the bucket created earlier
-        policy: siteBucket.bucket.apply(publicReadPolicyForBucket) // use output property `siteBucket.bucket`
-    });
-
+    
+    // CDN endpoint to the website.
+    // Allow it some time after the deployment to get ready.
+     const cdnEndpoint = pulumi.interpolate`https://${endpoint.hostName}/`;
+        
     return {
-        websiteUrl: siteBucket.websiteEndpoint,
+        websiteUrl: cdnEndpoint,
     };
 };
 // creates new sites
@@ -168,7 +195,7 @@ const deleteHandler: express.RequestHandler = async (req, res) => {
 };
 const ensurePlugins = async () => {
     const ws = await LocalWorkspace.create({});
-    await ws.installPlugin("aws", "v4.0.0");
+    await ws.installPlugin("azure", "5.11.0");
 };
 
 // install necessary plugins once upon boot
